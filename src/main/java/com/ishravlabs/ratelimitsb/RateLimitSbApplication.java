e1d763baeb9ec04e2b8a101cf82439c8b32e9eea
ishara <ishara9@gmail.com>
2023-10-01 08:14:56 +0530

    first commit

---
+++ src/main/java/com/ishravlabs/ratelimitsb/RateLimitSbApplication.java
@@ -0,0 +1,13 @@
+package com.ishravlabs.ratelimitsb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RateLimitSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RateLimitSbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ishravlabs/ratelimitsb/RateLimitSbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ishravlabs.ratelimitsb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RateLimitSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
