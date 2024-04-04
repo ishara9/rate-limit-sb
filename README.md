f5f43e5b57a7b839ce2f530912a03fc88094e872
ishara <ishara9@gmail.com>
2024-04-04 11:21:44 +0530

    POST made work and added README.md

---
+++ README.md
@@ -0,0 +1,13 @@
+## Setup
+
+### Use Rate Limit
+
+- Call POST method `localhost:8080/api/v1/rectangle/area`
+- sample body
+```
+{
+    "length": 10,
+    "width": 5
+}
+```
+- Header `X-api-key` with value starting `BX001-*` 
--- src/main/java/com/ishravlabs/ratelimitsb/secuirity/SecurityConfig.java
+++ src/main/java/com/ishravlabs/ratelimitsb/secuirity/SecurityConfig.java
@@ -6,7 +6,6 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -17,25 +16,27 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
   @Override
   protected void configure(AuthenticationManagerBuilder auth) throws Exception {
     auth.inMemoryAuthentication()
-            .withUser("foo")
-            .password("foo")
-            .roles("USER")
-            .and()
-            .withUser("fee")
-            .password("fee")
-            .roles("ADMIN");
+        .withUser("foo")
+        .password("foo")
+        .roles("USER")
+        .and()
+        .withUser("fee")
+        .password("fee")
+        .roles("ADMIN");
   }
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http.authorizeRequests()
-            .antMatchers("/admin").hasAnyRole("ADMIN")
-            .antMatchers("/user").hasAnyRole("USER", "ADMIN")
-            .antMatchers("/**").permitAll()
-            .and()
+        .antMatchers("/admin").hasAnyRole("ADMIN")
+        .antMatchers("/user").hasAnyRole("USER", "ADMIN")
+        .antMatchers("/**").permitAll()
+        .and()
+        .csrf()
+        .disable()
 //            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 //            .and()
-            .formLogin();
+        .formLogin();
   }
 
   @Bean
